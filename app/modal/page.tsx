d1d024d32c3fc44c5658e845d9970fcc38d70e8d
Chandra Bose <chandrabosep3112@gmail.com>
2024-03-28 22:34:26 +0530

    feat: chat functionality

--- app/modal/page.tsx
+++ app/modal/page.tsx
@@ -1,9 +1,18 @@
 "use client";
-import React from "react";
+
+import React, { useState } from "react";
+import { Form, FormField, FormItem } from "../_components/ui/form";
 import { Input } from "../_components/ui/input";
 import { Button } from "../_components/ui/button";
 import { Bot, User } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { useAccount } from "wagmi";
+
+const formSchema = z.object({
+  inputTxt: z.string().min(2).max(50),
+});
 
 export default function Page() {
   const { address } = useAccount();
@@ -17,6 +26,30 @@ export default function Page() {
     );
   }
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      inputTxt: "",
+    },
+  });
+
+  const [messages, setMessages] = useState<{ role: string; content: string }[]>(
+    []
+  );
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const newMessages: { role: string; content: string }[] = [
+      ...messages,
+      {
+        role: "user",
+        content: values.inputTxt,
+      },
+    ];
+    setMessages(newMessages);
+
+    form.reset();
+  }
+
   return (
     <div className="h-full max-h-full relative overflow-y-clip">
       <div className="space-y-1.5 px-6 border-b border-[#444444] py-3">
@@ -27,28 +60,49 @@ export default function Page() {
       </div>
       <div className="w-[98%] h-[90%] mx-auto flex flex-col">
         <div className="flex-1 overflow-scroll py-8 space-y-10">
-          <div className="max-w-2xl flex items-center gap-4">
-            <div className="p-3 bg-[#3f3f46] rounded-full">
-              <User />
-            </div>
-            <p>Hello</p>
-          </div>
-          <div className="max-w-2xl flex items-center gap-4">
-            <div className="p-3 bg-blue-500 rounded-full">
-              <Bot />
-            </div>
-            <p>Hello</p>
+          <div className="flex-1 overflow-scroll py-8 space-y-10">
+            {messages.reverse().map(({ role, content }, index) => (
+              <div key={index} className="max-w-2xl flex items-center gap-4">
+                {role === "user" ? (
+                  <div className="p-3 bg-[#3f3f46] rounded-full">
+                    <User />
+                  </div>
+                ) : (
+                  <div className="p-3 bg-blue-500 rounded-full">
+                    <Bot />
+                  </div>
+                )}
+                <p>{content}</p>
+              </div>
+            ))}
           </div>
         </div>
-        <div className="flex gap-4 mto sticky bottom-0 w-full">
-          <Input
-            placeholder="Start typing..."
-            className="w-full mt-auto  bg-transparent border-[#444444] focus-visible:ring-0 focus-visible:ring-offset-0 ring-0"
-          ></Input>
-          <Button className="bg-blue-500 hover:bg-blue-600 rounded-xl">
-            Search
-          </Button>
-        </div>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex gap-4 mto sticky bottom-0 w-full"
+          >
+            <FormField
+              control={form.control}
+              name="inputTxt"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <Input
+                    {...field}
+                    placeholder="Search here..."
+                    className="w-full mt-auto  bg-transparent border-[#444444] focus-visible:ring-0 focus-visible:ring-offset-0 ring-0"
+                  />
+                </FormItem>
+              )}
+            />
+            <Button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-600 rounded-xl"
+            >
+              Search
+            </Button>
+          </form>
+        </Form>
       </div>
     </div>
   );
